4b892eeb1861bf44780b6b002ad1d7ffde4aea74
Tobias Andersson <anderssontobias88@gmail.com>
2017-02-25 15:26:15 +0100

    WIP

--- bin/install.sh
+++ bin/install.sh
@@ -7,15 +7,6 @@ yes | apt install $(awk -F: '/ubuntu/ {print $2}' ~/README.md)
 #PYTHON
 pip3 install powerline-status
 
-#GIT
-mkdir ~/repos
-cd ~/repos
-
-for git in $(awk -F- '/.*\.git/ && !/dotfiles/ {print $2}' ~/README.md)
-do
-    git clone $git
-done
-
 #PPA
 for ppa in $(awk -F- '/ppa:/ {print $2}' ~/README.md)
 do
@@ -24,4 +15,13 @@ done
 
 apt update
 yes | apt install ambiance-flat-colors numix-icon-theme oracle-java8-installer
+
+#GIT
+mkdir ~/repos
+cd ~/repos
+
+for git in $(awk -F- '/.*\.git/ && !/dotfiles/ {print $2}' ~/README.md)
+do
+    git clone $git
+done
 
